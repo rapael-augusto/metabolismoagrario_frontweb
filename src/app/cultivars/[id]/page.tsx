fd7cffabd598aafa8c6dc8b9346f8133e6ecba33
Lucas Melo <lmelo072002@gmail.com>
2024-06-08 12:13:41 -0300

    page Listar Cultivares concluida

--- src/app/cultivars/[id]/page.tsx
+++ src/app/cultivars/[id]/page.tsx
@@ -1,39 +1,40 @@
-'use client';
+"use client";
 
 import Layout from "@/components/layout/layout";
-import "../../styles/crops/pageCrops.css"
-import { cropsService } from "@/services/crops";
+import "../../../styles/crops/pageCrops.css"
+import "../../../styles/cultivar/pageCultivar.css"
 import { useEffect, useState } from "react";
 import { redirect } from "next/navigation";
+import { cropsService } from "@/services/crops";
 import Image from "next/image";
 import NavButton from "@/components/layout/navigationButton";
-import { dataCropsType } from "@/types/cropsTypes";
 
-const Crops = () => {
-    const [dados, setDados] = useState<dataCropsType[] | any>([])
+interface Props {
+    params: { id: string }
+}
 
-    // const TraducaoClimas: any = {
-    //     "TropicalRainforest": "Floresta tropical",
-    //     "Tropical": "Tropical",
-    //     "Subtropical": "Subtropical",
-    //     "Desert": "Deserto",
-    //     "Temperate": "Temperado",
-    //     "Mediterranean": "Mediterrâneo",
-    //     "SemiArid": "Semi-árido",
-    //     "Subpolar": "Subpolar",
-    //     "MountainCold": "Frio da montanha",
-    //     "Polar": "Polar",
-    // }
+type cultivarsData = {
+    id: string
+    cropId: string
+    name: string
+}
+
+const Cultivars = ({ params }: Props) => {
+    const [dados, setDados] = useState<cultivarsData[]>([])
+    const [titulo, setTitulo] = useState<string | any>('')
 
     useEffect(() => {
         let session = sessionStorage.getItem('@token')
-        if (session != null) {
 
-            const crops = new cropsService(session)
+        if (session) {
+            const service = new cropsService(session)
 
-            crops.list().then((response) => {
-                setDados(response)
+            service.findOne(params.id).then((response) => {
+                setDados(response.cultivars)
+                setTitulo(response.name)
             })
+
+
         } else {
             sessionStorage.setItem('mensagem', `{"mensagem":"Você não possui permissões para acessar essa pagina !","tipo":"danger"}`)
             redirect('/')
@@ -44,40 +45,34 @@ const Crops = () => {
     return (
         <Layout>
             <div className="cropsPage">
-                <h2 className="titulo-crops" >Lista de culturas</h2>
+                <h2 className="titulo-crops">Cultivares de {titulo}</h2>
 
-
-                <div className="list-crops">
+                <div className="list-cultivars">
                     <div className="container-button-crops">
-                        <NavButton Url="/home" text={"Voltar"} type="voltar" page="list" />
-                        <div>
-                            <NavButton Url="/criarCrops" text={"Cadastrar Cultura"} type="cadastrar" page="list" />
-                        </div>
+                        <NavButton Url={"/crops"} page="list" text="Voltar" type="voltar" />
+                        <NavButton Url={`/criarCultivar/${params.id}`} page="list" text="Cadastrar Constante" type="cadastrar" />
                     </div>
 
                     <div className="header-list">
 
-                        <div className="header-col-name">
+                        <div className="header-col-nameCultivar">
                             Nome
                         </div>
-                        <div className="header-col-cientific-name">
-                            Nome científico
-                        </div>
-                        <div className="header-col-acoes">
+
+                        <div className="header-col-acoesCultivar">
                             Ações
                         </div>
+
                     </div>
                     {
-                        dados.map((e: dataCropsType) => (
+                        dados.map((e: cultivarsData) => (
                             <div key={e.id} className="content-list">
-                                <div className="result-col-name">
+                                <div className="result-col-nameCultivar">
                                     {e.name}
                                 </div>
-                                <div className="result-col-cientific-name">
-                                    {e.scientificName}
-                                </div>
-                                <div className="result-col-acoes">
-                                    <a href={`/cultivars/${e.id}`}>
+
+                                <div className="result-col-acoesCultivar">
+                                    <a href={`/constant/${e.id}`}>
                                         <Image
                                             src={"/visualizar.svg"}
                                             alt="visualizar"
@@ -99,14 +94,16 @@ const Crops = () => {
                                         height={20}
                                     />
                                 </div>
+
                             </div>
                         ))
                     }
                 </div>
-
             </div>
+
+
         </Layout>
     );
 }
 
-export default Crops;
+export default Cultivars;
