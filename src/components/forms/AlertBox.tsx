88ab7ede2efe1fcc03b0b65631550cd9d04fdecb
Igordcs <igorcostamdt@gmail.com>
2024-11-14 13:38:57 -0300

    formatação

--- src/components/forms/AlertBox.tsx
+++ src/components/forms/AlertBox.tsx
@@ -1,32 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const AlertBox: React.FC = () => {
-    const [isVisible, setIsVisible] = useState(false);
-    const [message, setMessage] = useState('');
+  const [isVisible, setIsVisible] = useState(false);
+  const [message, setMessage] = useState("");
 
-    useEffect(() => {
-        const mensagem = sessionStorage.getItem('mensagem');
-        if (mensagem) {
-            setMessage(JSON.parse(mensagem).mensagem);
-            setIsVisible(true);
+  useEffect(() => {
+    const mensagem = sessionStorage.getItem("mensagem");
+    if (mensagem) {
+      setMessage(JSON.parse(mensagem).mensagem);
+      setIsVisible(true);
 
-            sessionStorage.removeItem('mensagem');
+      sessionStorage.removeItem("mensagem");
 
-            const timer = setTimeout(() => {
-                setIsVisible(false);
-            }, 2000);
+      const timer = setTimeout(() => {
+        setIsVisible(false);
+      }, 2000);
 
-            return () => clearTimeout(timer);
-        }
-    }, []);
+      return () => clearTimeout(timer);
+    }
+  }, []);
 
-    return (
-        isVisible && (
-            <div className="alert-box">
-                <p>{message}</p>
-            </div>
-        )
-    );
+  return (
+    isVisible && (
+      <div className="alert-box">
+        <p>{message}</p>
+      </div>
+    )
+  );
 };
 
 export default AlertBox;
